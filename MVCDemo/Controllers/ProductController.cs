e0ade77a05eaefe4db5580ce8f46bd3b153efa47
Sandipan Ghosh <sandipan.g@zensar.com>
2020-08-20 21:09:36 +0530

    mvc assignment on PRODUCTS for 18/8/2020 and 19/8/2020

---
+++ MVCDemo/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var product = GetProduct();
+            return View(product);
+        }
+        public IEnumerable<Product>GetProduct()
+        {
+            return new List<Product>()
+            {
+                new Product{Id=1,Name="Mouse",Rate=100.01 },
+                new Product{Id=2,Name="Keyboard",Rate=200.01 },
+                new Product{Id=3,Name="Laptop",Rate=10000.01 },
+                new Product{Id=4,Name="Pendrive",Rate=150.01 },
+            };
+        }
+        public ActionResult Details(int id)
+        {
+            var product = GetProduct().SingleOrDefault(c => c.Id == id);
+            if (product == null)
+                return HttpNotFound();
+            else
+                return View(product);
+        }
+    }
+}
